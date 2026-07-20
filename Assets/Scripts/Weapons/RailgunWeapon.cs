38ef0c296dce3149c8323153579e0d91109496ea
DtheMakeeeR <danilgol45@mail.ru>
2026-07-20 15:24:13 +0300

    collider fix + spread

--- Assets/Scripts/Weapons/RailgunWeapon.cs
+++ Assets/Scripts/Weapons/RailgunWeapon.cs
@@ -7,12 +7,27 @@ namespace GardenDefense
         [SerializeField]
         private float _knockbackForce = 1f;
         [SerializeField]
+        private float _accuracySpread = 0f;
+        [SerializeField]
         public LayerMask shootableLayers;
         protected override void Shoot()
         {
             Debug.Log($"{gameObject.name} make shot");
             RaycastHit hit;
-            if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, range, shootableLayers))
+            Vector3 shootOrigin = Camera.main.transform.position;
+            Vector3 shootDirection = Camera.main.transform.forward;
+
+            if (_accuracySpread > 0)
+            {
+                shootDirection = Quaternion.Euler(
+                    Random.Range(-_accuracySpread, _accuracySpread),
+                    Random.Range(-_accuracySpread, _accuracySpread),
+                    0
+                ) * shootDirection;
+            }
+
+            Debug.DrawRay(shootOrigin, shootDirection * range, Color.green, 2f);
+            if (Physics.Raycast(shootOrigin, shootDirection, out hit, range, shootableLayers))
             {
                 Debug.Log($"Hit: {hit.collider.name}");
                 var health = hit.collider.GetComponent<Entity>();
@@ -23,9 +38,10 @@ namespace GardenDefense
                 var rb = hit.collider.GetComponent<Rigidbody>();
                 if (rb != null)
                 {
-                    rb.AddForce(-hit.normal * _knockbackForce); 
+                    rb.AddForce(-hit.normal * _knockbackForce);
                 }
             }
+            else Debug.Log($"Hit: nothing");
         }
     }
 }
